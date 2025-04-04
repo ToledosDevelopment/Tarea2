db7a550037ccb5a29105d868ce3ff7d0d4c1458f
Obed Toledo <toledo.programmer@hotmail.com>
2025-04-04 10:38:38 -0600

    Resolved merge comflicts and table helper para presentar la información en tablas

--- Tarea2.py
+++ Tarea2.py
@@ -11,6 +11,20 @@ plot_img_dir = "plt_images"
 contour_txt_dir = "contour_txt_images"
 morph_dir = "morph_images"
 
+import TableHelper
+from JSONHelper import JSONHelper
+
+# obtenemos las imágenes de la carpeta de imágenes
+imgList =  os.listdir("imagenes")[0:5]
+images = []
+for imPath in imgList:
+    with Image.open(os.path.join("imagenes", imPath)) as im:
+        im = im.convert("1")
+        im.id = ImageHelper.getImageID(im)
+        images.append(im)
+
+# definimos la imagen base para el escalado de las imágenes
+baseImage = images[0]
 
 
 # ImageHelper.getTxtImagesFromFolder(img_dir, txt_dir)
@@ -28,3 +42,15 @@ morph_dir = "morph_images"
 # Punto 8
 
 MorphologyHelper.getMorphImagesFromFolder(txt_dir,morph_dir)
+
+# trasladamos las imágenes y obtenemos las invariantes de mu o momentos centrales
+translatedImages, muInvariants, muInvariantsTranslated, translations = ImageHelper.translateImagesAndGetInvariants(images)
+TableHelper.presentMuInvariantsOnTable(imgList, translations, muInvariants, muInvariantsTranslated)
+
+# escalamos las imágenes y obtenemos los invariantes eta
+scaledImages, etaInvariants, etaInvariantsScaled, scales = ImageHelper.scaleImagesAndGetInvariants(baseImage, images)
+TableHelper.presentEtaInvariantsOnTable(imgList, scales, etaInvariants, etaInvariantsScaled)
+
+# rotamos las imágenes y obtenemos los invariantes de phi
+scaledImages, phiInvariants, phiInvariantsScaled, degrees = ImageHelper.rotateImagesAndGetInvariants(images)
+TableHelper.presentPhiInvariantsOnTable(imgList, degrees, phiInvariants, phiInvariantsScaled)
