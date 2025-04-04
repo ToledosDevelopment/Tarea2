a07f10b968cff9814044c21aaf91cc0fb3e6b5f1
Obed Toledo <toledo.programmer@hotmail.com>
2025-04-04 10:35:46 -0600

    Revert "Agregamos el helper que nos ayuda a presentar toda la información de las invariantes en tablas"
    This reverts commit 4586cc5a3e7d3af851985b809a958359de91ea2e.

--- Tarea2.py
+++ Tarea2.py
@@ -2,29 +2,15 @@ from PIL import Image
 import numpy as np
 import os
 import ImageHelper
-import TableHelper
 from JSONHelper import JSONHelper
 
-# obtenemos las imágenes de la carpeta de imágenes
-imgList =  os.listdir("imagenes")[0:5]
-images = []
-for imPath in imgList:
-    with Image.open(os.path.join("imagenes", imPath)) as im:
-        im = im.convert("1")
-        im.id = ImageHelper.getImageID(im)
-        images.append(im)
+imgList =  os.listdir("imagenes")[0:2]
+images = [Image.open(os.path.join("imagenes", img)) for img in imgList]
 
-# definimos la imagen base para el escalado de las imágenes
+for im in images:
+    im.id = ImageHelper.getImageID(im)
+
 baseImage = images[0]
+print(ImageHelper.ScaleImagesToEqualOnePixels(baseImage, images))
 
-# trasladamos las imágenes y obtenemos las invariantes de mu o momentos centrales
-translatedImages, muInvariants, muInvariantsTranslated, translations = ImageHelper.translateImagesAndGetInvariants(images)
-TableHelper.presentMuInvariantsOnTable(imgList, translations, muInvariants, muInvariantsTranslated)
 
-# escalamos las imágenes y obtenemos los invariantes eta
-scaledImages, etaInvariants, etaInvariantsScaled, scales = ImageHelper.scaleImagesAndGetInvariants(baseImage, images)
-TableHelper.presentEtaInvariantsOnTable(imgList, scales, etaInvariants, etaInvariantsScaled)
-
-# rotamos las imágenes y obtenemos los invariantes de phi
-scaledImages, phiInvariants, phiInvariantsScaled, degrees = ImageHelper.rotateImagesAndGetInvariants(images)
-TableHelper.presentPhiInvariantsOnTable(imgList, degrees, phiInvariants, phiInvariantsScaled)
